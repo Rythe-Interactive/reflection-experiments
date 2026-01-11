c435162165d0216231f97487b1f3858bbbcd54b6
Glyn Leine <g.m.leine@gmail.com>
2026-01-11 22:26:41 +0100

    add target_class2 back in

--- applications/reflection-experiments/src/target/test.hpp
+++ applications/reflection-experiments/src/target/test.hpp
@@ -22,3 +22,13 @@ public:
 private:
     char char_variable;
 };
+
+class target_class2
+{
+public:
+    int int_variable2;
+
+    void void_function2(int int_parameter);
+private:
+    char char_variable2;
+};
